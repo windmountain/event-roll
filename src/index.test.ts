8b30c4a57944286d7131bbb6517130cc382fd09d
Zach Rose <zach@windmountainsoftware.com>
2024-10-28 14:27:46 -0400

    TDD out a first pass of this design

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,12 @@
 import { afterEach, beforeEach, describe, expect, it, vi, test} from 'vitest'
-import { Player } from "./index";
+import { Deck } from "./index";
 
 
 const mock = vi.fn();
 
-const events = [{
+const tape = {
+  length: 5000,  
+  events: [{
   t: 2000,
   d: "Al"
 },{
@@ -22,9 +24,10 @@ const events = [{
 },{
   t: 4000,
   d: "call, "
-}];
+}]
+}
 
-describe("Player", () => {
+describe("Playing a tape", () => {
   
   beforeEach(() => {
     vi.useFakeTimers()
@@ -35,7 +38,7 @@ describe("Player", () => {
   });
 
   it("should play events like so", () => {
-    const player = new Player(mock).load(events).play();
+    const player = new Deck(mock).load(tape).play();
     expect(mock).not.toHaveBeenCalled();
     vi.advanceTimersByTime(2001);
     expect(mock).toHaveBeenCalledTimes(1);
@@ -57,4 +60,48 @@ describe("Player", () => {
     expect(mock).toHaveBeenLastCalledWith("call, ");
   });
 
+});
+
+describe("Recording onto a tape", () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it("should record events like so", () => {
+    const emptyTape = {
+      length: 5000,
+      events: []
+    }
+    const deck = new Deck(mock).load(emptyTape);
+    const recorder = deck.record((rawEvent) => "bop");
+    vi.advanceTimersByTime(2000);
+    recorder(null);
+    vi.advanceTimersByTime(63);
+    recorder(null);
+    vi.advanceTimersByTime(125);
+    recorder(null);
+    vi.advanceTimersByTime(63);
+    recorder(null);
+    vi.advanceTimersByTime(63);
+    recorder(null);
+    vi.advanceTimersByTime(1686);
+    recorder(null);
+    deck.stop();
+    const tape = deck.eject();
+    expect(tape).toEqual({
+      length: 5000,
+      events: [
+        { d: "bop", t: 2000 },
+        { d: "bop", t: 2063 },
+        { d: "bop", t: 2188 },
+        { d: "bop", t: 2251 },
+        { d: "bop", t: 2314 },
+        { d: "bop", t: 4000 },
+      ]
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,83 +2,175 @@ export function player(fn: (d: any) => void) {
   return `Hello !`;
 }
 
-interface Event {
-  d: any;
-  t: number;
-}
-
 declare function setTimeout(handler: (...args: any[]) => void, timeout: number): any;
 declare function clearTimeout(handle: any): void;
 
 type Timer = ReturnType<typeof setTimeout>
 
-export class Player {
-  doer: (d: any) => void;
-  events: Array<Event> | null;
-  timeouts: Array<Timer>;
-  playhead: number;
-
-  constructor(doer: (d: any) => void) {
-    this.doer = doer;
-    this.events = null
-    this.timeouts = [];
-    this.playhead = 0;
-  };
-
-  load(events: Array<Event>) {
-    this.events = events;
-    return this;
-  };
-
-  play() {
-    if(this.events == null) {
-      throw "problem";
-    }
-    this.timeouts = this.events
-      .filter((event: Event) => {
-        return event.t >= this.playhead;
-      })
-      .map((event: Event) => {
-        return setTimeout(() => {
-          this.doer(event.d)
-        }, event.t - this.playhead);
-      });
-  };
-
-  pause() {
-    this.timeouts.forEach((timeoutId: number) => {
-      clearTimeout(timeoutId);
-    });
-  };
-
-  seekTo(t: number) {
-    this.playhead = t;
-  };
+interface Event<D> {
+  d: D;
+  t: number;
 }
 
-export class Recorder {
-  taker: (d: any) => void;
-  events: Array<Event>;
+type Tape<D> = {
+  events: Array<Event<D>>,
+  length: number
+}
 
-  constructor(taker: (d: any) => void) {
-    this.taker = taker;
-    this.events = [];
+enum TapeState {
+    Playing,
+    Recording,
+    Stopped
+  }
+
+type DeckState = 
+  { state: TapeState.Stopped, at: number } |
+  { state: TapeState.Playing, from: number, since: Date, timeouts: Array<Timer> } |
+  { state: TapeState.Recording, from: number, since: Date }
+
+class NoTapeError extends Error {
+  constructor() {
+    super("No tape in deck")
+    this.name = "NoTapeError"
+    Object.setPrototypeOf(this, NoTapeError.prototype);
+  }
+}
+
+function unreachable(value: never) {}
+
+export class Deck<D> {
+  perform: (d: D) => void;
+  tape: null | Tape<D>;
+  state: DeckState;
+
+  constructor(perform : ((d: D) => void)) {
+    this.tape = null;
+    this.perform = perform;
+    this.state = { state: TapeState.Stopped, at: 0 }
   };
 
-  load(events: Array<Event>) {
-    this.events = events;
+  load(tape: Tape<D>) {
+    this.tape = tape;
     return this;
   };
 
-  record() {
-
+  eject() : null | Tape<D> {
+    return this.tape;
   }
 
-  pause() {
-
+  _newAt() : number {
+    switch(this.state.state) {
+      case TapeState.Stopped:
+          return this.state.at;
+      case TapeState.Playing:
+      case TapeState.Recording:
+          return (new Date().getTime() - this.state.since.getTime()) + this.state.from;
+    }
   }
 
-  seekTo() {
-
+  play() {
+    if(!this.tape) {
+      throw new NoTapeError();
+    }
+    const at = this._newAt();
+    switch(this.state.state) {
+      case TapeState.Playing:
+        return;
+      case TapeState.Stopped:
+      case TapeState.Recording:
+        this.state = {
+          state: TapeState.Playing,
+          from: at,
+          since: new Date(),
+          timeouts: this.tape.events.map((e) => setTimeout(() =>
+            this.perform(e.d),
+            e.t - at
+          ))
+        }
+    }
   }
+
+  stop() {
+    if(!this.tape) {
+      throw new NoTapeError();
+    }
+    switch(this.state.state) {
+      case TapeState.Playing:
+        this.state.timeouts.map((timeout) => clearTimeout(timeout));
+        this.state = { state: TapeState.Stopped, at: this._newAt() }
+      case TapeState.Stopped:
+      case TapeState.Recording:
+        this.state = { state: TapeState.Stopped, at: this._newAt() }
+    }
+  }
+
+  rewind() {
+    if(!this.tape) {
+      throw new NoTapeError();
+    }
+    this.state = { state: TapeState.Stopped, at: 0 }
+  }
+
+  fastForward() {
+    if(!this.tape) {
+      throw new NoTapeError();
+    }
+    this.state = { state: TapeState.Stopped, at: this.tape.length }
+  }
+
+  record<R>(recorder: ((rawEvent: R) => D)) : ((rawEvent: R) => void) {
+    if(!this.tape) {
+      throw new NoTapeError();
+    }
+    this.stop();
+    const since = new Date();
+    const from = this._newAt();
+    this.state = { state: TapeState.Recording, from: from, since: since }
+    const now = () => new Date().getTime() - since.getTime() + from;
+    return ((le) => {
+      if(!this.tape) {
+        throw new NoTapeError();
+      }
+      void this.tape.events.push({
+        t: now(),
+        d: recorder(le)
+      });
+    })
+  }
+
+  /*
+  playing -> stopped
+  playing -> recording
+  stopped -> playing
+  stopped -> recording
+  recording -> stopped
+  recording -> playing
+
+  buttons:
+    - rewind
+    - record
+    - stop
+    - play
+    - fast forward
+  
+  effects (state -> button -> state)
+    playing -> rewind -> stopped (at beginning)
+    playing -> record -> recording
+    playing -> stop -> stopped
+    playing -> play -> playing
+    playing -> fast forward -> stopped (at end)
+    stopped -> rewind -> stopped
+    stopped -> record -> recording
+    stopped -> stop -> stopped
+    stopped -> play -> playing
+    stopped -> fast forward -> stopped (at beginning)
+    recording -> rewind -> stopped
+    recording -> record -> recording
+    recording -> stop -> stopped
+    recording -> play -> playing
+    recording -> fast forward -> stopped (at end)
+  
+  ticket: add a 'seeking' state, to be reached with FF and REW
+  
+  */
 }
